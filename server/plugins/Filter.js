a9d64c9334c440f75bd00b939701d9ac03cb03bf
Dave McKenzie <1100348+davidmckenzie@users.noreply.github.com>
2018-11-20 16:45:18 +1100

    hide inactive options and add filter plugin

---
+++ server/plugins/Filter.js
@@ -0,0 +1,20 @@
+
+function run(event, scope, data, config, callback) {
+    if (config.ignoreAddress) {
+        if (data.address.match(new RegExp(config.ignoreAddress))) {
+            data.pluginData.ignore = true;
+            console.log('Filter: ignoring message due to regex match on address');
+        }
+    }
+    if (config.ignoreMessage) {
+        if (data.message.match(new RegExp(config.ignoreMessage))) {
+            data.pluginData.ignore = true;
+            console.log('Filter: ignoring message due to regex match on content');
+        }
+    }
+    callback(data);
+}
+
+module.exports = {
+    run: run
+}
