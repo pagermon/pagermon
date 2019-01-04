b30b8aabaae27bb6bd78ed653a9a6d934e33f92f
Nathanial Marsh <nathanial@marshy.com.au>
2019-01-04 21:43:54 +1100

    Start on Regex Replace Plugin

---
+++ server/plugins/RegexReplace.js
@@ -0,0 +1,22 @@
+/*
+
+Regex Replace
+Allows matching and replacing
+logger.main.error('RegexReplace: Invalid config setting - Please check your settings!')
+
+*/
+var logger = require('../log');
+function run(trigger, scope, data, config, callback) {
+    if (config.regexReplaceMatchRegex) {
+        if (data.message.match(new RegExp(config.regexReplaceMatchRegex))) {
+            logger.main.info('RegexReplace: Found a match, replacing it');
+						data.message.replace(config.regexReplaceMatchRegex, config.regexReplaceString)
+            logger.main.debug('RegexReplace: Message has change to: '  + data.message)
+        }
+    }
+    callback(data);
+}
+
+module.exports = {
+    run: run
+}
