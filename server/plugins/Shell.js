22e960252eb209a7e841019a9685ab9f30f5523c
All-3kcis <contact@all-3kcis.fr>
2019-02-03 16:18:41 +0100

    Passing data to scripts

--- server/plugins/Shell.js
+++ server/plugins/Shell.js
@@ -1,61 +1,52 @@
-var exec = require('child_process').exec;
+var spawn = require("child_process").spawn;
 var fs = require('fs');
 
 
 function run(trigger, scope, data, config, callback) {
 
-    var available_language = {
-      'sh':'sh',
-      'php':'/usr/bin/php',
-      'python':'/usr/bin/python',
-      'python3':'/usr/bin/python3',
-    };
-    var language_ext = {
-      'sh':'sh',
-      'php':'php',
-      'python':'py',
-      'python3':'py',
-    };
-    var used_program = 'sh'; // Default programm
-    var used_ext = 'sh'; // Default programm
+  if(data.pluginconf.Shell.enable){
+    var file_path = process.cwd()+'/plugins/Shell/';
+    var file_name = data.alias_id;
 
-    if(data.pluginconf.Shell.enable){
-
-      var file_path = process.cwd()+'/plugins/Shell/';
-      var file_name = data.alias_id+'.'+used_ext;
-
-      if(typeof data.pluginconf.Shell.language != "undefined" && typeof available_language[data.pluginconf.Shell.language.value] != "undefined"){
-        used_program = available_language[data.pluginconf.Shell.language.value];
-        used_ext = language_ext[data.pluginconf.Shell.language.value];
-      }
-
-      // Check file exist
-      if(data.pluginconf.Shell.overrideAlias > 0){
-        console.log('Override filename')
-        var file_name = data.pluginconf.Shell.overrideAlias+'.'+used_ext;
-      }
-
-      
-
-      
-   
-      if(fs.existsSync(file_path+file_name)){
-          console.log('Exec shell command for selected alias')
-          dir = exec(used_program + ' '+file_path+file_name, function(err, stdout, stderr) {
-            if (err) {
-              console.log('Shell Cmd: Error code :' + err);
-              console.log('Shell Cmd: ' + stderr);
-            }
-            console.log('Shell Cmd: ' + stdout);
-          });
-          dir.on('exit', function (code) {
-            console.log('Shell Cmd: exit code : ' + code);
-          });
-          //console.log('Shell Cmd: started.');
-      }else{
-        console.log('File '+file_name+' not exist');
-      }
+    // Override ID if needed
+    if(data.pluginconf.Shell.overrideAlias > 0){
+      console.log('Override filename')
+      var file_name = data.pluginconf.Shell.overrideAlias;
     }
+
+    if(process.platform === "win32"){
+      // Windows platform, Win 64bits too
+      var full_file_name = file_name+'.ps1';
+    }else{
+      // Others, 'aix', 'darwin', 'freebsd', 'linux', 'openbsd', 'sunos'
+      var full_file_name = file_name+'.sh';
+    }
+
+    // Check file exist
+    if(fs.existsSync(file_path+full_file_name)){
+        console.log('Exec shell command for selected alias')
+
+        if(process.platform === "win32"){
+          var child = spawn("powershell.exe", [file_path+full_file_name, data.address, data.message, JSON.stringify(data)]);
+        }else{
+          var child = spawn("sh", [file_path+full_file_name, data.address, data.message, JSON.stringify(data)]);
+        }
+
+        child.stdout.on("data",function(data){
+            console.log("ShellScript Data: " + data);
+        });
+        child.stderr.on("data",function(data){
+            console.log("ShellScript Errors: " + data);
+        });
+        child.on("exit",function(code){ // Exit code, ok = 0
+            console.log("ShellScript finished");
+        });
+        child.stdin.end(); //end input
+
+    }else{
+      console.log('File '+full_file_name+' not exist');
+    }
+  }
 }
 
 module.exports = {
