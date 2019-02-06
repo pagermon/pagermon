06390c4d86851aee2f3f6aede33120bc2122901f
Nathanial Marsh <nathanial@marshy.com.au>
2019-02-06 18:59:57 +1100

    Format everything nicely + cleanup

--- server/plugins/MessageRepeat.js
+++ server/plugins/MessageRepeat.js
@@ -19,21 +19,18 @@ function run (trigger, scope, data, config, callback) {
       url: uri,
       headers: {
         'X-Requested-With': 'XMLHttpRequest',
-        "content-type": "application/json",
+        "content-type": 'application/json',
         'User-Agent': 'PagerMon Plugin - Message Repeat',
         apikey: apikey,
         json: true
       },
       form: messageData
     }, function (error, response, body) {
-      //if (!error && response.statusCode === 200) {
-        //logger.main.info('MessageRepeat: Message Sent')
-      //} else {
-      //  logger.main.error('MessageRepeat: ' + error + response.statusCode + response.statusText)
-      //}
-      console.log('error:', error); // Print the error if one occurred
-console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-console.log('body:', body); // Print the HTML for the Google homepage.
+      if (!error && response.statusCode === 200) {
+        logger.main.info('MessageRepeat: Message Sent')
+      } else {
+        logger.main.error('MessageRepeat: ' + error + response.statusCode + response.statusText)
+      }
     })
     callback()
   } else {
