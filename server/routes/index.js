4109bac1e1820cdc04b0b4ead6adf32567eab7a6
Daniel Williams <daniel@echelon.id.au>
2019-03-24 13:27:53 +1100

    Add's ability to select Icon Size on homepage

--- server/routes/index.js
+++ server/routes/index.js
@@ -14,6 +14,7 @@ router.use(function (req, res, next) {
   res.locals.hidecapcode = nconf.get('messages:HideCapcode');
   res.locals.hidesource = nconf.get('messages:HideSource');
   res.locals.apisecurity = nconf.get('messages:apiSecurity');
+  res.locals.iconsize = nconf.get('messages:iconsize')
   next();
 });
 
