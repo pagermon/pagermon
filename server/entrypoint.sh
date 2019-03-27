4addcd4079b103279563edb7d57cc23a68396478
Nick Pratley <nickp@Nicks-MacBook-Pro.local>
2019-03-27 20:37:27 +1100

    adding support for persistant configs / copying default config on startup

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -8,5 +8,10 @@ else
     # create file on first run, use existing file thereafter
     mkdir -p /data && touch /data/messages.db && ln -s "/data/messages.db" ./messages.db
 fi
+if [ -f /data/config.json ]; then
+    ln -s "/data/config.json" ./config/config.json
+else
+    cp ./config/default.json /data/config.json && ln -s "/data/config.json" ./config/config.json
+fi
 
 node app.js
