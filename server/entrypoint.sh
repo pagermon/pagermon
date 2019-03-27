a344cf4c6db882c61dcf770ab876232655e60bc5
Nick Pratley <nickp@Nicks-MacBook-Pro.local>
2019-03-27 22:37:11 +1100

    s/cat/sed/ on process-default.json so only a single file needs to change

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -23,43 +23,21 @@ if [ -z "$APP_NAME" ]; then
   APP_NAME=pagermon
 fi
 
-rm -rf /app/process-default.json
+## Make required changes to process-default.json for environment variable support
+sed -i "/cwd/c\   \"cwd\" : \"/app\"," /app/process-default.json
+sed -i "/NODE_ENV/c\   \"NODE_ENV\" : \"$NODE_ENV\"," /app/process-default.json
+sed -i "/HOSTNAME/c\   \"HOSTNAME\" : \"$HOSTNAME\"," /app/process-default.json
+sed -i "/USE_COOKIE_HOST/c\   \"USE_COOKIE_HOST\" : $USE_COOKIE_HOST," /app/process-default.json
+sed -i "/APP_NAME/c\   \"APP_NAME\" : \"$APP_NAME\"" /app/process-default.json
 
-cat <<EOT > /app/process-default.json
-{
-  "name"             : "pagermon",
-  "cwd"              : "/app",
-  "script"           : "app.js",
-  "node_args"        : ["--harmony"],
-  "error_file"       : "logs/node-app.stderr.log",
-  "out_file"         : "logs/node-app.stdout.log",
-  "pid_file"         : "pids/node-geo-api.pid",
-  "instances"        : 1, //or 0 => 'max'
-  "min_uptime"       : "200s", // 200 seconds, defaults to 1000
-  "max_restarts"     : 10, // defaults to 15
-  "max_memory_restart": "100M", // 1 megabytes, e.g.: "2G", "10M", "100K", 1024 the default unit is byte.
-  "cron_restart"     : "1 0 * * *",
-  "watch"            : false,
-  "ignore_watch"      : ["[\\/\\\\]\\./", "node_modules", "config", "logs"],
-  "merge_logs"       : true,
-  "exec_interpreter" : "node",
-  "autorestart"      : true, // enable/disable automatic restart when an app crashes or exits
-  "vizion"           : false, // enable/disable vizion features (versioning control)
-  "env": {
-    "NODE_ENV": "$NODE_ENV",
-    "HOSTNAME": "$HOSTNAME",
-    "USE_COOKIE_HOST": $USE_COOKIE_HOST,
-    "APP_NAME": "$APP_NAME"
-  }
-}
-EOT
-
+## Create messages database if it doesn't exist
 if [ -f /data/messages.db ]; then
     ln -s "/data/messages.db" ./messages.db
 else
-    # create file on first run, use existing file thereafter
     mkdir -p /data && touch /data/messages.db && ln -s "/data/messages.db" ./messages.db
 fi
+
+## Create and link default config.json file, if it doesn't exist
 if [ -f /data/config.json ]; then
     ln -s "/data/config.json" ./config/config.json
 else
