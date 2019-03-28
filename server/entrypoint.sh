eea67d47a56c940a8f7b32c0ad9fa623db7d0098
Nick Pratley <nick@npratley.net>
2019-03-28 12:31:18 +1100

    Proper support for MySQL with auto-configure from default.env variables

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -23,6 +23,8 @@ if [ -z "$APP_NAME" ]; then
   APP_NAME=pagermon
 fi
 
+## Configure environment.
+
 ## Make required changes to process-default.json for environment variable support
 sed -i "/cwd/c\   \"cwd\" : \"/app\"," /app/process-default.json
 sed -i "/error_file/c\   \"error_file\" : \"../data/logs/node-app.stderr.log\"," /app/process-default.json
@@ -33,18 +35,23 @@ sed -i "/HOSTNAME/c\   \"HOSTNAME\" : \"$HOSTNAME\"," /app/process-default.json
 sed -i "/USE_COOKIE_HOST/c\   \"USE_COOKIE_HOST\" : $USE_COOKIE_HOST," /app/process-default.json
 sed -i "/APP_NAME/c\   \"APP_NAME\" : \"$APP_NAME\"" /app/process-default.json
 
-## Create messages database if it doesn't exist
-if [ -f /data/messages.db ]; then
-    ln -s "/data/messages.db" ./messages.db
-else
-    mkdir -p /data && touch /data/messages.db && ln -s "/data/messages.db" ./messages.db
-fi
-
 ## Create and link default config.json file, if it doesn't exist
+rm -rf /app/config/config.json #Shoud never be local config here, so here we'll kill it just incase it's been left over / otherwise linking will fail
 if [ -f /data/config.json ]; then
-    ln -s "/data/config.json" ./config/config.json
+    ln -s /data/config.json /app/config/config.json
 else
-    cp ./config/default.json /data/config.json && ln -s "/data/config.json" ./config/config.json
+node > /data/config.json <<EOF
+var data = require('/app/config/default.json');
+
+data.database.type = 'mysql';
+data.database.server = '$MYSQL_HOST';
+data.database.username = '$MYSQL_USER';
+data.database.password = '$MYSQL_PASSWORD';
+data.database.database = '$MYSQL_DATABASE';
+
+console.log(JSON.stringify(data, null, 4));
+EOF
+ln -s /data/config.json ./config/config.json
 fi
 
 ## Create default process.json file for pm2 server
@@ -54,4 +61,14 @@ else
     cp ./process-default.json /data/process.json && ln -s "/data/process.json" ./process.json
 fi
 
-pm2 start process.json --no-daemon
+## We probably should wait for MySQL to come up
+echo "=== Waiting for MySQL to come up ==="
+while ! mysqladmin ping -h $MYSQL_HOST --silent; do
+    sleep 1
+done
+echo "=== MySQL is up =="
+
+#knex migrate:latest
+
+node app.js
+#pm2 start process.json --no-daemon
