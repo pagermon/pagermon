988d800129818c56acfd7a5563c9d6225134dd6c
Daniel Williams <daniel@echelon.id.au>
2019-05-07 20:51:14 +1000

    i lied

--- server/knex/migrations/20190507000000_mysql_foreign.js
+++ server/knex/migrations/20190507000000_mysql_foreign.js
@@ -11,6 +11,7 @@ exports.up = function(db, Promise) {
             // This is here to fix original broken mysql installs - probably not required going forward.
             db.schema.table('messages', function (table) {
                 table.dropForeign('alias_id');
+                table.dropColumn('alias_id');
                 table.integer('alias_id').unsigned()
                 table.foreign('alias_id').references('id').inTable('capcodes').onUpdate('CASCADE').onDelete('CASCADE');
             })
