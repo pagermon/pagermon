d8ee6bab0aa858c46c856241a7e926067eb3bb8b
Daniel Williams <daniel@echelon.id.au>
2019-05-07 21:04:00 +1000

    Ok your annoying me now

--- server/knex/migrations/20190507000000_mysql_foreign.js
+++ server/knex/migrations/20190507000000_mysql_foreign.js
@@ -16,6 +16,12 @@ exports.up = function(db, Promise) {
                 db.schema.table('messages', function (table) {
                     table.integer('alias_id').unsigned().references('id').inTable('capcodes').onUpdate('CASCADE').onDelete('CASCADE');
                  })
+                 .then (
+
+                 )
+                 .catch (
+                     
+                 )
             ).catch (
                 
             )
