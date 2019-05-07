93f6abd2622a4ed706b7c936f5fe24bfcbe1aaf7
Daniel Williams <daniel@echelon.id.au>
2019-05-07 21:38:15 +1000

    Cleanup

--- server/knex/migrations/20190322204706_create_messages_table.js
+++ server/knex/migrations/20190322204706_create_messages_table.js
@@ -3,6 +3,8 @@ exports.up = function(db, Promise) {
     return db.schema.hasTable('messages').then(function(exists) {
         if (!exists) {
             return db.schema.createTable('messages', table => {
+                table.charset(utf8);
+                table.collate(utf8_general_ci);
                 table.increments('id').primary().unique().notNullable();
                 table.string('address', [255]).notNullable();
                 table.text('message').notNullable();
