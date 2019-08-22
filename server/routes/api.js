18b03c57025fc3cc54fdf7c6fbd19f900a41aa32
Daniel Williams <daniel@echelon.id.au>
2019-08-22 18:36:32 +1000

    Fix capcodeCheck leak

--- server/routes/api.js
+++ server/routes/api.js
@@ -1002,7 +1002,7 @@ function inParam (sql, arr) {
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
   if (req.method == 'GET') { 
-    if (apiSecurity || req.url.match(/capcodes/i) && !(req.url.match(/agency$/))) { //check if Secure mode is on, or if the route is a capcode route
+    if (apiSecurity || ((req.url.match(/capcodes/i) || req.url.match(/capcodeCheck/i)) && !(req.url.match(/agency$/))) ) { //check if Secure mode is on, or if the route is a capcode route
       if (req.isAuthenticated()) {
         // if user is authenticated in the session, carry on
         return next();
