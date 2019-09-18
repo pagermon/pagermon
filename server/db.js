afd82c81701c1571817cf1cdd0f4fff3da533f04
Daniel Williams <26653344+DanrwAU@users.noreply.github.com>
2019-09-18 18:11:56 +1000

    Update db.js

--- server/db.js
+++ server/db.js
@@ -21,13 +21,14 @@ function init() {
     if (dbtype == 'sqlite3') {
         // Legacy Datbase handling - force an upgrade and or remove the old version numbers
         db.raw(`pragma user_version;`).then(function (res) {
-            logger.main.debug("Current DB version: " + res[0].user_version);
             // Check if database is currently v0.2.3 if not force upgrade to that first
             if (res[0].user_version < 20181118 && res[0].user_version != 0) {
+                logger.main.info("Current Legacy DB version: " + res[0].user_version);
                 logger.main.error("Unsupported Upgrade Version - Upgrade Pagermon Database to v0.2.3 BEFORE upgrading to v0.3.0");
                 process.exit(1)
             } else if (res[0].user_version > 20181118) {
             // If the database has a legacy version number from 0.3.0 - remove it    
+                logger.main.info("Current Legacy DB version: " + res[0].user_version);
                 var vervar = 'pragma user_version = 0;'
                 db.raw(vervar)
                 .then((result) => {
@@ -37,6 +38,11 @@ function init() {
                     logger.main.error('Error removing legacy DB version infomation' + err)
                 })
             }
+        })
+        db.migrate.currentVersion().then((result) => {
+            logger.main.info("Current DB version: " + result);
+        }).catch((err) => {
+            logger.main.error('Error retrieving database version' + err)
         })
         logger.main.info('Checking for database upgrades')
         db.migrate.latest()
