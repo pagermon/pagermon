ca0ee3d90a406390795efc1727f38e73f9a0c42d
Dave McKenzie <1100348+davidmckenzie@users.noreply.github.com>
2019-11-18 16:13:31 +1100

    err better

--- server/routes/api.js
+++ server/routes/api.js
@@ -433,10 +433,12 @@ router.get('/capcodes', isLoggedIn, function(req, res, next) {
     .select('*')
     //.orderByRaw("REPLACE(address, '_', '%')")
     .then((rows) => {
+      res.status(200);
       res.json(rows);
     })
     .catch((err) => {
-      return next(err)
+      res.status(500);
+      res.send(err);
     })
 });
 
