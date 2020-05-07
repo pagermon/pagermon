0bc46e22db54be246967cdb0d776ffc8d9d77e7c
Daniel Williams <26653344+DanrwAU@users.noreply.github.com>
2020-05-07 19:41:04 +1000

    Chain all the promises

--- server/knex/migrations/20200507165756_messages_foreignkey_fix.js
+++ server/knex/migrations/20200507165756_messages_foreignkey_fix.js
@@ -5,17 +5,18 @@ exports.up = function (db, Promise) {
     if (dbtype == 'mysql') {
         return db.schema.table('messages', function (table) {
             table.dropForeign('alias_id')
-        }).then(function () {
-            return db.schema.table('messages', function (table) {
-                table.dropColumn('alias_id')
-            }
-                .then(function () {
-                    return db.schema.table('table', function (table) {
-                        table.integer('alias_id').unsigned().references('id').inTable('capcodes').onDelete('SET NULL');
-                        nconf.set('database:aliasRefreshRequired', 1);
-                        nconf.save();
-                    })
+        })
+            .then(function () {
+                return db.schema.table('messages', function (table) {
+                    table.dropColumn('alias_id')
                 })
+                    .then(function () {
+                        return db.schema.table('table', function (table) {
+                            table.integer('alias_id').unsigned().references('id').inTable('capcodes').onDelete('SET NULL');
+                            nconf.set('database:aliasRefreshRequired', 1);
+                            nconf.save();
+                        })
+                    })
         })
     } else {
         return Promise.resolve('Not Required')
