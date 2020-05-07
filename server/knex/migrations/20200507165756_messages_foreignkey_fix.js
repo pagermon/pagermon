ff0c65a4a0e84b3ea23f8839d1a1257b73228a00
Daniel Williams <26653344+DanrwAU@users.noreply.github.com>
2020-05-07 20:32:13 +1000

    oopsie

--- server/knex/migrations/20200507165756_messages_foreignkey_fix.js
+++ server/knex/migrations/20200507165756_messages_foreignkey_fix.js
@@ -11,9 +11,8 @@ exports.up = function (db, Promise) {
                     table.dropColumn('alias_id')
                 })
                     .then(function () {
-                        return db.schema.table('', function (table) {
-                            table.integer('alias_id').unsigned().references('id').inTable('capcodes').onDelete('SET NULL');
-                            
+                        return db.schema.table('messages', function (table) {
+                            table.integer('alias_id').unsigned().references('id').inTable('capcodes').onDelete('SET NULL'); 
                         })
                         .then(function () {
                             nconf.set('database:aliasRefreshRequired', 1);
