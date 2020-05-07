2a4b347acf185dfba86ffd86ed959730a10641c7
Daniel Williams <26653344+DanrwAU@users.noreply.github.com>
2020-05-07 19:24:34 +1000

    Add alter statements

--- server/knex/migrations/20200507165756_messages_foreignkey_fix.js
+++ server/knex/migrations/20200507165756_messages_foreignkey_fix.js
@@ -3,8 +3,8 @@ var conf_file = './config/config.json';
 var dbtype = nconf.get('database:type')
 exports.up = function(db, Promise) {
     if (dbtype == 'mysql') {
-    return db.schema.table('messages', function(table) {
-        table.integer('alias_id').unsigned().references('id').inTable('capcodes').onDelete('SET NULL');
+    return db.schema.alterTable('messages', function(table) {
+        table.integer('alias_id').unsigned().references('id').inTable('capcodes').onDelete('SET NULL').alter();
       })
     } else {
         return Promise.resolve('Not Required')
