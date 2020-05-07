f071ef08f9220c960e08f9600eb626681f2a8a6b
Daniel Williams <26653344+DanrwAU@users.noreply.github.com>
2020-05-07 19:18:53 +1000

    Add fix for multi delete not working on MySQL

---
+++ server/knex/migrations/20200507165756_messages_foreignkey_fix.js
@@ -0,0 +1,16 @@
+var nconf = require('nconf');
+var conf_file = './config/config.json';
+var dbtype = nconf.get('database:type')
+exports.up = function(db, Promise) {
+    if (dbtype == 'mysql') {
+    return db.schema.table('messages', function(table) {
+        table.integer('alias_id').unsigned().references('id').inTable('capcodes').onDelete('SET NULL');
+      })
+    } else {
+        return Promise.resolve('Not Required')
+    }
+};
+
+exports.down = function(knex, Promise) {
+  
+};
