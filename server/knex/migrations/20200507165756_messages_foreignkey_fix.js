537e535f937662a896d22b55f1aaf53599d1fe6d
Daniel Williams <26653344+DanrwAU@users.noreply.github.com>
2020-05-07 19:36:02 +1000

    MOAR PROMISES

--- server/knex/migrations/20200507165756_messages_foreignkey_fix.js
+++ server/knex/migrations/20200507165756_messages_foreignkey_fix.js
@@ -1,19 +1,23 @@
 var nconf = require('nconf');
 var conf_file = './config/config.json';
 var dbtype = nconf.get('database:type')
-exports.up = function(db, Promise) {
+exports.up = function (db, Promise) {
     if (dbtype == 'mysql') {
-    return db.schema.alterTable('messages', function(table) {
-        table.dropColumn('alias_id')
-        table.integer('alias_id').unsigned().references('id').inTable('capcodes').onDelete('SET NULL');
-        nconf.set('database:aliasRefreshRequired', 1);
-        nconf.save();
-      })
+        return db.schema.table('messages', function (table) {
+            table.dropColumn('alias_id')
+        })
+            .then(function () {
+                return db.schema.table('table', function (table) {
+                    table.integer('alias_id').unsigned().references('id').inTable('capcodes').onDelete('SET NULL');
+                    nconf.set('database:aliasRefreshRequired', 1);
+                    nconf.save();
+                })
+            })
     } else {
         return Promise.resolve('Not Required')
     }
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+
 };
