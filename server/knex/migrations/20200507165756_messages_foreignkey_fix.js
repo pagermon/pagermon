baf24163d30ae53b646d48fffef9921d3183c9f5
Daniel Williams <26653344+DanrwAU@users.noreply.github.com>
2020-05-07 19:39:37 +1000

    Databases make my head hurt

--- server/knex/migrations/20200507165756_messages_foreignkey_fix.js
+++ server/knex/migrations/20200507165756_messages_foreignkey_fix.js
@@ -4,15 +4,19 @@ var dbtype = nconf.get('database:type')
 exports.up = function (db, Promise) {
     if (dbtype == 'mysql') {
         return db.schema.table('messages', function (table) {
-            table.dropColumn('alias_id')
-        })
-            .then(function () {
-                return db.schema.table('table', function (table) {
-                    table.integer('alias_id').unsigned().references('id').inTable('capcodes').onDelete('SET NULL');
-                    nconf.set('database:aliasRefreshRequired', 1);
-                    nconf.save();
+            table.dropForeign('alias_id')
+        }).then(function () {
+            return db.schema.table('messages', function (table) {
+                table.dropColumn('alias_id')
+            }
+                .then(function () {
+                    return db.schema.table('table', function (table) {
+                        table.integer('alias_id').unsigned().references('id').inTable('capcodes').onDelete('SET NULL');
+                        nconf.set('database:aliasRefreshRequired', 1);
+                        nconf.save();
+                    })
                 })
-            })
+        })
     } else {
         return Promise.resolve('Not Required')
     }
