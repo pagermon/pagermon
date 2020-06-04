a360432676629abdb4d7b980ac76fbcef60c42d0
Daniel Williams <26653344+DanrwAU@users.noreply.github.com>
2020-06-04 15:21:42 +1000

    eslint

--- server/test/routes.auth.test.js
+++ server/test/routes.auth.test.js
@@ -1,100 +1,94 @@
 process.env.NODE_ENV = 'test';
 
 const chai = require('chai');
+
 const should = chai.should();
 const chaiHttp = require('chai-http');
+
 chai.use(chaiHttp);
 
 const server = require('../app');
-var db = require('../knex/knex.js');
-//This needs to be sorted out, use a different config file when testing?
-var conf_file = './config/config.json';
+const db = require('../knex/knex.js');
+// This needs to be sorted out, use a different config file when testing?
+const conf_file = './config/config.json';
 // load the config file
-var nconf = require('nconf');
-    nconf.file({file: conf_file});
-    nconf.load();
+const nconf = require('nconf');
+
+nconf.file({ file: conf_file });
+nconf.load();
 // set required settings in config file
 nconf.set('auth:registration', true);
 nconf.save();
 
 describe('GET /auth/register', () => {
-  it('should return the registration page if enabled', (done) => {
-      chai
-        .request(server)
-        .get('/auth/register')
-        .end((err, res) => {
-          should.not.exist(err);
-          res.status.should.eql(200);
-          res.type.should.eql('text/html');
-          done();
+        it('should return the registration page if enabled', done => {
+                chai.request(server)
+                        .get('/auth/register')
+                        .end((err, res) => {
+                                should.not.exist(err);
+                                res.status.should.eql(200);
+                                res.type.should.eql('text/html');
+                                done();
+                        });
         });
-    });
-  });
+});
 
 describe('POST /auth/register', () => {
-  beforeEach(() => {
-    return db.migrate.rollback()
-    .then(() => { return db.migrate.latest(); });
-  });
+        beforeEach(() => db.migrate.rollback().then(() => db.migrate.latest()));
 
-  afterEach(() => {
-    return db.migrate.rollback();
-  });
-  it('should register a new user', (done) => {
-      chai
-        .request(server)
-        .post('/auth/register')
-        .send({
-          username: 'test',
-          password: '$2a$08$De/aXnQkZIEbQ9p8J22tHuzLltqIbsAxE2CGgRMPLaaIwwHmVrpsu',
-          givenname: 'Test',
-          surname: 'User',
-          email: 'Test@test.com',
-        })
-        .end((err, res) => {
-          should.not.exist(err);
-          res.status.should.eql(200);
-          res.type.should.eql('application/json');
-          res.body.status.should.eql('ok');
-          done();
+        afterEach(() => db.migrate.rollback());
+        it('should register a new user', done => {
+                chai.request(server)
+                        .post('/auth/register')
+                        .send({
+                                username: 'test',
+                                password: '$2a$08$De/aXnQkZIEbQ9p8J22tHuzLltqIbsAxE2CGgRMPLaaIwwHmVrpsu',
+                                givenname: 'Test',
+                                surname: 'User',
+                                email: 'Test@test.com',
+                        })
+                        .end((err, res) => {
+                                should.not.exist(err);
+                                res.status.should.eql(200);
+                                res.type.should.eql('application/json');
+                                res.body.status.should.eql('ok');
+                                done();
+                        });
         });
-    });
-    it('should not register a duplicate user', (done) => {
-      chai
-        .request(server)
-        .post('/auth/register')
-        .send({
-          username: 'admin',
-          password: '$2a$08$De/aXnQkZIEbQ9p8J22tHuzLltqIbsAxE2CGgRMPLaaIwwHmVrpsu',
-          givenname: 'Admin',
-          surname: 'User',
-          email: 'Test@test.com',
-        })
-        .end((err, res) => {
-          res.status.should.eql(401);
-          res.type.should.eql('application/json');
-          res.body.error.should.eql('access denied');
-          done();
+        it('should not register a duplicate user', done => {
+                chai.request(server)
+                        .post('/auth/register')
+                        .send({
+                                username: 'admin',
+                                password: '$2a$08$De/aXnQkZIEbQ9p8J22tHuzLltqIbsAxE2CGgRMPLaaIwwHmVrpsu',
+                                givenname: 'Admin',
+                                surname: 'User',
+                                email: 'Test@test.com',
+                        })
+                        .end((err, res) => {
+                                res.status.should.eql(401);
+                                res.type.should.eql('application/json');
+                                res.body.error.should.eql('access denied');
+                                done();
+                        });
         });
-    });
-    it('should not allow registration when registration is disabled in config', (done) => {
-      nconf.set('auth:registration', false);
-      nconf.save();
-      chai
-        .request(server)
-        .post('/auth/register')
-        .send({
-          username: 'test',
-          password: '$2a$08$De/aXnQkZIEbQ9p8J22tHuzLltqIbsAxE2CGgRMPLaaIwwHmVrpsu',
-          givenname: 'Admin',
-          surname: 'User',
-          email: 'Test@test.com',
-        })
-        .end((err, res) => {
-          res.status.should.eql(400);
-          res.type.should.eql('application/json');
-          res.body.error.should.eql('registration disabled');
-          done();
+        it('should not allow registration when registration is disabled in config', done => {
+                nconf.set('auth:registration', false);
+                nconf.save();
+                chai.request(server)
+                        .post('/auth/register')
+                        .send({
+                                username: 'test',
+                                password: '$2a$08$De/aXnQkZIEbQ9p8J22tHuzLltqIbsAxE2CGgRMPLaaIwwHmVrpsu',
+                                givenname: 'Admin',
+                                surname: 'User',
+                                email: 'Test@test.com',
+                        })
+                        .end((err, res) => {
+                                res.status.should.eql(400);
+                                res.type.should.eql('application/json');
+                                res.body.error.should.eql('registration disabled');
+                                done();
+                        });
         });
-    });
-  });
+});
