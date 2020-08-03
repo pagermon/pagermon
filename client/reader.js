19ecc19a58a4c5148fe4c8ead0ea3317992b1a9b
eopo <26900313+eopo@users.noreply.github.com>
2020-08-03 05:13:59 +0200

    Fix #426
    Rewrote major parts of reader.js
    Put handling for different protocols into seperate functions
    Modified FLEX handling to process new multimon group message format introduced with EliasOenal/multimon-ng@46c4a53eeb07b8e0f8e371897983ddea4b470997 - Awaiting EliasOenal/multimon-ng#155 response to see if done right.
    Hopefully fixes #426

--- client/reader.js
+++ client/reader.js
@@ -1,7 +1,7 @@
 //
 // PagerMon - reader.js
-// 2017-06-04
-// Author: Dave McKenzie
+// 2020-07-25
+// Author: Dave McKenzie and the Pagermon contributors
 //
 // Description: Takes output of multimon-ng and pushes to PagerMon server
 //
@@ -11,158 +11,213 @@
 // Example: reader.sh
 //
 
-// CONFIG
-// create config file if it does not exist, and set defaults
-var fs = require('fs');
-var conf_defaults = require('./config/default.json');
-var conf_file = './config/config.json';
+//PACKAGE LOADING
+const fs = require('fs');
+const nConf = require('nconf');
+require('request').debug = true;
+const rp = require('request-promise-native');
+const moment = require('moment');
+const colors = require('colors/safe');
+const readline = require('readline');
+
+
+// CONFIG AND SETUP
+// Create config file if it does not exist, and set defaults
+const conf_defaults = require('./config/default.json');
+const conf_file = './config/config.json';
 if( ! fs.existsSync(conf_file) ) {
     fs.writeFileSync( conf_file, JSON.stringify(conf_defaults,null, 2) );
     console.log('created config file - set your api key in '+conf_file);
     return;
 }
-// load the config file
-var nconf = require('nconf');
-    nconf.file({file: conf_file});
-    nconf.load();
 
-var hostname = nconf.get('hostname');
-var apikey = nconf.get('apikey');
-var identifier = nconf.get('identifier');
-var sendFunctionCode = nconf.get('sendFunctionCode') || false;
-var useTimestamp = nconf.get('useTimestamp') || true;
+// Load the existing config file
+nConf.file({file: conf_file});
+nConf.load();
+
+const hostname = nConf.get('hostname');
+const apikey = nConf.get('apikey');
+const identifier = nConf.get('identifier');
+const sendFunctionCode = nConf.get('sendFunctionCode') || false;
+const useTimestamp = nConf.get('useTimestamp') || true;
+let uri;
 
 //Check if hostname is in a valid format - currently only removes trailing slash - possibly expand to validate the whole URI? 
 if(hostname.substr(-1) === '/') {
-  var uri = hostname.substr(0, hostname.length - 1)+'/api/messages';
+  uri = hostname+'api/messages';
 } else {
-  var uri = hostname+'/api/messages'
+  uri = hostname+'/api/messages'
 }
 
-var http = require('http');
-var request = require('request');
-require('request').debug = true;
-var rp = require('request-promise-native');
-var moment = require('moment');
-
-var colors = require('colors/safe');
+//Commandline color setup
 colors.setTheme({
   success: ['white', 'bold', 'bgBlue'],
   error: ['red', 'bold', 'bgwhite']
 });
 
-const readline = require('readline');
-
+//Readline setup
 const rl = readline.createInterface({
     input: process.stdin,
     terminal: true
 });
 
-var frag = {};
+//Variable initialization
+let frags = {};
+let messages = [];
 
+//MESSAGE HANDLING
 rl.on('line', (line) => {
   //console.log(`Received: ${line.trim()}`);
-  var time = moment().format("YYYY-MM-DD HH:mm:ss");
-  var timeString = '';
-  var datetime = moment().unix();
-  var address;
-  var message;
-  var trimMessage;
+  let lineObj = {
+    time: moment().format("YYYY-MM-DD HH:mm:ss"),
+    datetime: moment().unix(),
+    line: line
+  }
   // TODO: pad address with zeros for better address matching
-//  if (line.indexOf('POCSAG512: Address:') > -1) {	
+//  if (line.indexOf('POCSAG512: Address:') > -1) {
+
   if (/POCSAG(\d+): Address: /.test(line) ) {
-    address = line.match(/POCSAG(\d+): Address:(.*?)Function/)[2].trim();
-    if (sendFunctionCode) {
-      address += line.match(/POCSAG(\d+): Address:(.*?)Function: (\d)/)[3];
-    }
-    if (line.indexOf('Alpha:') > -1) {
-      message = line.match(/Alpha:(.*?)$/)[1].trim();
-      if (useTimestamp) {
-        if (message.match(/\d{2} \w+ \d{4} \d{2}:\d{2}:\d{2}/)) {
-          timeString = message.match(/\d+ \w+ \d+ \d{2}:\d{2}:\d{2}/)[0];
-          if (moment(timeString, 'DD MMMM YYYY HH:mm:ss').isValid()) {
-            datetime = moment(timeString, 'DD MMMM YYYY HH:mm:ss').unix();
-            message = message.replace(/\d{2} \w+ \d{4} \d{2}:\d{2}:\d{2}/,'');
-          }
-        } else if (message.match(/\d+-\d+-\d+ \d{2}:\d{2}:\d{2}/)) {
-          timeString = message.match(/\d+-\d+-\d+ \d{2}:\d{2}:\d{2}/)[0];
-          if (moment(timeString).isValid()) {
-            datetime = moment(timeString).unix();
-            message = message.replace(/\d+-\d+-\d+ \d{2}:\d{2}:\d{2}/, '');
-          }
-        }
-      }
-      trimMessage = message.replace(/<[A-Za-z]{3}>/g,'').replace(/Ä/g,'[').replace(/Ü/g,']').trim();
-    } else if (line.indexOf('Numeric:') > -1) {
-      message = line.match(/Numeric:(.*?)$/)[1].trim();
-      trimMessage = message.replace(/<[A-Za-z]{3}>/g,'').replace(/Ä/g,'[').replace(/Ü/g,']');
-    } else {
-      message = false;
-      trimMessage = '';
-    }
-  } else if (line.match(/FLEX[:|]/)) {
-    address = line.match(/FLEX[:|] ?.*?[\[|](\d*?)[\]| ]/)[1].trim();
-    if (useTimestamp) {
-      if (line.match(/FLEX[:|] ?\d{2} \w+ \d{4} \d{2}:\d{2}:\d{2}/)) {
-        timeString = line.match(/\d+ \w+ \d+ \d{2}:\d{2}:\d{2}/)[0];
-        if (moment(timeString, 'DD MMMM YYYY HH:mm:ss').isValid()) {
-          datetime = moment(timeString, 'DD MMMM YYYY HH:mm:ss').unix();
-        }
-      } else if (line.match(/FLEX[:|] ?\d+-\d+-\d+ \d{2}:\d{2}:\d{2}/)) {
-        timeString = line.match(/\d+-\d+-\d+ \d{2}:\d{2}:\d{2}/)[0];
-        if (moment(timeString).isValid()) {
-          datetime = moment(timeString).unix();
-        }
-      }
-    }
-    if (line.match( /([ |]ALN[ |]|[ |]GPN[ |]|[ |]NUM[ |])/ )) {
-      message = line.match(/FLEX[:|].*[|\[][0-9 ]*[|\]] ?...[ |](.+)/)[1].trim();
-      if (line.match( /[ |][0-9]{4}\/[0-9]\/F\/.[ |]/ )) {
-        // message is fragmented, hold onto it for next line
-        frag[address] = message;
-        message = false;
-        trimMessage = '';
-      } else if (line.match( /[ |][0-9]{4}\/[0-9]\/C\/.[ |]/ )) {
-        // message is a completion of the last fragmented message
-        trimMessage = frag[address]+message;
-        delete frag[address];
-      } else if (line.match( /[ |][0-9]{4}\/[0-9]\/K\/.[ |]/ )) {
-        // message is a full message
-        trimMessage = message;
-      } else {
-        // message doesn't have the KFC flags, treat as full message
-        trimMessage = message;
-      }
-    }
+    messages = messages.concat(pocsagHandler(lineObj));
+  } else if (/FLEX[:|]/.test(line)) {
+    messages = messages.concat(flexHandler(lineObj));
   } else {
-    address = '';
-    message = false;
-    trimMessage = '';
+    console.log(colors.red(lineObj.time+' - No protocol found: ')+colors.grey(lineObj.line));
   }
 
   // filter out most false hits
   // if too much junk data, make sure '-p' option isn't enabled in multimon
-  if (address.length > 2 && message) {
-    var padAddress = padDigits(address,7);
-    console.log(colors.red(time+': ')+colors.yellow(padAddress+': ')+colors.success(trimMessage));
-    // now send the message
-    var form = {
-      address: padAddress,
-      message: trimMessage,
-      datetime: datetime,
-      source: identifier
-    };
-    sendPage(form, 0);
-  } else {
-    console.log(colors.red(time+': ')+colors.grey(line));
+  while (messages.length > 0) {
+    let message = messages.shift();
+    if (message.address.length > 2 && message.message) {
+      message.address = padDigits(message.address,7);
+      console.log(colors.red(message.datetime+': ')+colors.yellow(message.protocol+'- '+message.address+': ')+colors.white(message.message));
+      // now send the message
+      const form = {
+        ...message,
+        source: identifier
+      };
+      sendPage(form, 0);
+    }
+    else {
+      console.log(colors.red(message.time+': ')+colors.grey(line));
+    }
   }
-  
+
 }).on('close', () => {
   console.log('Input died!');
 });
 
-var sendPage = function(message,retries) {
-  var options = {
+//PROTOCOL HANDLING
+const pocsagHandler = function(lineObj) {
+  let message = {
+    protocol: 'Pocsag',
+    address: lineObj.line.match(/POCSAG(\d+): Address:(.*?)Function/)[2].trim(),
+  };
+  if (sendFunctionCode) {
+    message.address += lineObj.line.match(/POCSAG(\d+): Address:(.*?)Function: (\d)/)[3];
+  }
+  if (lineObj.line.indexOf('Alpha:') > -1) {
+    message.type = 'Alphanumeric';
+    message.message = lineObj.line.match(/Alpha:(.*?)$/)[1].trim();
+    if (useTimestamp) {
+      if (message.message.match(/\d{2} \w+ \d{4} \d{2}:\d{2}:\d{2}/)) {
+        let timeString = message.message.match(/\d+ \w+ \d+ \d{2}:\d{2}:\d{2}/)[0];
+        if (moment(timeString, 'DD MMMM YYYY HH:mm:ss').isValid()) {
+          message = {
+            ...message,
+            datetime: moment(timeString, 'DD MMMM YYYY HH:mm:ss').unix(),
+            message: message.message.replace(/\d{2} \w+ \d{4} \d{2}:\d{2}:\d{2}/,'')
+          };
+        }
+      } else if (message.message.match(/\d+-\d+-\d+ \d{2}:\d{2}:\d{2}/)) {
+        let timeString = message.message.match(/\d+-\d+-\d+ \d{2}:\d{2}:\d{2}/)[0];
+        if (moment(timeString).isValid()) {
+          message = {
+            ...message,
+            datetime: moment(timeString).unix(),
+            message: message.message.replace(/\d+-\d+-\d+ \d{2}:\d{2}:\d{2}/, '')
+          }
+        }
+      }
+    }
+    //TODO: Do we need this part? Multimon has a charset function since a year or so, so the error that this fixed should not occur anymore
+    message.message = message.replace(/<[A-Za-z]{3}>/g,'').replace(/Ä/g,'[').replace(/Ü/g,']').trim();
+  }
+  else if (lineObj.indexOf('Numeric:') > -1) {
+    message = {
+      ...message,
+      message: lineObj.line.match(/Numeric:(.*?)$/)[1].trim()
+    }
+    //TODO: Do we need this part? Multimon has a charset function since a year or so, so the error that this fixed should not occur anymore
+    message.message = message.message.replace(/<[A-Za-z]{3}>/g,'').replace(/Ä/g,'[').replace(/Ü/g,']');
+  }
+  else {
+    message = {...message,
+      type: 'Empty',
+      message: null
+    };
+  }
+  return [message];
+}
+const flexHandler = function(lineObj) {
+  let messages = [];
+  let tempMessage = {
+    protocol: 'FLEX',
+    addressString: lineObj.line.match(/FLEX[:|] ?.*?[\[|]([\d ]*)[\]| ]/)[1].trim()
+  }
+
+  if (useTimestamp) {
+    if (lineObj.line.match(/FLEX[:|] ?\d{2} \w+ \d{4} \d{2}:\d{2}:\d{2}/)) {
+      let timeString = lineObj.line.match(/\d+ \w+ \d+ \d{2}:\d{2}:\d{2}/)[0];
+      if (moment(timeString, 'DD MMMM YYYY HH:mm:ss').isValid()) {
+        tempMessage.datetime = moment(timeString, 'DD MMMM YYYY HH:mm:ss').unix();
+      }
+    }
+    else if (lineObj.line.match(/FLEX[:|] ?\d+-\d+-\d+ \d{2}:\d{2}:\d{2}/)) {
+      let timeString = lineObj.line.match(/\d+-\d+-\d+ \d{2}:\d{2}:\d{2}/)[0];
+      if (moment(timeString).isValid()) {
+        tempMessage.datetime = moment(timeString).unix();
+      }
+    }
+  }
+
+  if (lineObj.line.match( /([ |]ALN[ |]|[ |]GPN[ |]|[ |]NUM[ |])/ )) {
+    if (lineObj.line.match( /[ |][0-9]{4}\/[0-9]\/F\/.[ |]/ )) {
+      // message is fragmented, hold onto it for next line
+      tempMessage.message = lineObj.line.match(/FLEX[:|].*[|\[][0-9 ]*[|\]] ?...[ |](.+)/)[1].trim();
+      frags[tempMessage.addressString] = tempMessage;
+    }
+    else if (lineObj.line.match( /[ |][0-9]{4}\/[0-9]\/C\/.[ |]/ )) {
+      // message is a completion of the last fragmented message
+      tempMessage.message = frags[tempMessage.addressString]+tempMessage.message;
+      delete frags[tempMessage.addressString];
+    } /* Todo: Not needed, since redundant with part below. Maybe keep for future use.
+    else if (lineObj.line.match( /[ |][0-9]{4}\/[0-9]\/K\/.[ |]/ )) {
+      // message is a full message
+      tempMessage.message = trimMessage;
+    }
+    Todo: Not needed, since fragmentation handling does only jump in, when needed. Otherwise, message is set directly.
+    else {
+      // message doesn't have the KFC flags, treat as full message
+      tempMessage.message = trimMessage;
+    }
+     **/
+  }
+
+  let addresses = tempMessage.addressString.split(' ');
+  delete tempMessage.addressString;
+  addresses.forEach((address) => {
+    messages.push({
+      ...tempMessage,
+      address: address
+    });
+  });
+  return messages;
+}
+
+//HELPER FUNCTION DEFINITIONS
+const sendPage = function(message,retries) {
+  const options = {
     method: 'POST',
     uri: uri,
     headers: {
@@ -179,16 +234,17 @@ var sendPage = function(message,retries) {
   .catch(function (err) {
     console.log(colors.yellow('Message failed to deliver. '+err));
     if (retries < 10) {
-      var retryTime = Math.pow(2, retries) * 1000;
+      const retryTime = Math.pow(2, retries) * 1000;
       retries++;
       console.log(colors.yellow(`Retrying in ${retryTime} ms`));
       setTimeout(sendPage, retryTime, message, retries);
-    } else {
+    }
+    else {
       console.log(colors.yellow('Message failed to deliver after 10 retries, giving up'));
     }
   });
-};
+}
 
-var padDigits = function(number, digits) {
-    return Array(Math.max(digits - String(number).length + 1, 0)).join(0) + number;
-};
+const padDigits = function(number, digits) {
+    return Array(Math.max(digits - String(number).length + 1, 0)).join('0') + number;
+}
