ca0224f35ae981f88e23b68700be87eb7bc1dc84
eopo <26900313+eopo@users.noreply.github.com>
2022-06-26 01:51:45 +0200

    Up to ES2015 - for app.js

--- server/app.js
+++ server/app.js
@@ -13,9 +13,11 @@ const favicon = require('serve-favicon');
 const flash = require('connect-flash');
 const http = require('http');
 const logger = require('./log');
+const morgan = require('morgan');
 const passport = require('./auth/local');
 const path = require('path');
 const session = require('express-session');
+const socketio = require('socket.io');
 const SQLiteStore = require('connect-sqlite3')(session);
 
 // TODO: variable io is never used, but later overwritten by socket.io
@@ -35,18 +37,8 @@ process.on('SIGINT', function() {
         process.exit(1);
 });
 
-// Load current theme
-let theme = config.get('global:theme');
-// TODO: set the theme if none found, for backwards compatibility
-if (!theme) {
-        config.set('global:theme', 'default');
-        config.save();
-        theme = config.get('global:theme');
-}
-
 // TODO: extract to seperate component
 // Enable Azure Monitoring if enabled
-
 if (config.get('monitoring:azureEnable')) {
         const azureKey = config.get('monitoring:azureKey');
         logger.main.debug('Starting Azure Application Insights');
@@ -67,90 +59,97 @@ if (config.get('monitoring:azureEnable')) {
 
 dbinit.init({ logger, db, config });
 
-const port = normalizePort(config.get('PORT'));
+const port = normalizePort(config.any('port', 'PORT'));
 
-var app = express();
-    app.set('port', port);
-    // view engine setup
-    app.set('views', path.join(__dirname,'themes',theme, 'views'));
-    app.set('view engine', 'ejs');
-    app.set('trust proxy', 'loopback, linklocal, uniquelocal');
+const app = express();
 
+app.set('port', port);
+// view engine setup
+app.set('views', path.join(__dirname, 'themes', config.get('global:theme'), 'views'));
+app.set('view engine', 'ejs');
+app.set('trust proxy', 'loopback, linklocal, uniquelocal');
 
+const server = http.createServer(app);
+const ioServer = socketio.listen(server);
 
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
-    server.listen(port);
-    server.on('error', onError);
-    server.on('listening', onListening);
-    //Set connection timeout to prevent long running queries failing on large databases - mostly capacode refresh on MySQL
-    server.on('connection', function(connection) {
-      connection.setTimeout(600 * 1000);
-    });
-    //Lets set setMaxListeners to a decent number - not to high to allow the memory leak warking to still trigger
-    io.sockets.setMaxListeners(20);
-io.sockets.on('connection', function (socket) {
-    socket.removeAllListeners();
-    debug('client connect to normal socket');
-//    socket.on('echo', function (data) {
-//        io.sockets.emit('message', data);
-//        console.log('message', data);
-//    });
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+// Set connection timeout to prevent long running queries failing on large databases - mostly capacode refresh on MySQL
+server.on('connection', function(connection) {
+        connection.setTimeout(600 * 1000);
 });
-//Admin Socket
-var adminio = io.of('/adminio');
-adminio.on('connection', function (socket) {
-    socket.removeAllListeners();
-    debug('client connect to admin socket');
-//    adminio.on('echo', function (data) {
-//        adminio.emit('message', data);
-//        console.log('message', data);
-//    });
+// Lets set setMaxListeners to a decent number - not to high to allow the memory leak warking to still trigger
+ioServer.sockets.setMaxListeners(20);
+ioServer.sockets.on('connection', function(socket) {
+        socket.removeAllListeners();
+        debug('client connect to normal socket');
+        // TODO: What is it, and do we need it?
+        //    socket.on('echo', function (data) {
+        //        io.sockets.emit('message', data);
+        //        console.log('message', data);
+        //    });
 });
 
-app.use(favicon(path.join(__dirname,'themes',theme, 'public', 'favicon.ico')));
+// Admin Socket
+const adminSocket = ioServer.of('/adminio');
+adminSocket.on('connection', function(socket) {
+        socket.removeAllListeners();
+        debug('client connect to admin socket');
+        // TODO: Same here
+        //    adminio.on('echo', function (data) {
+        //        adminio.emit('message', data);
+        //        console.log('message', data);
+        //    });
+});
+
+app.use(favicon(path.join(__dirname, 'themes', config.get('global:theme'), 'public', 'favicon.ico')));
 
 // set socket.io to be shared across all modules
-app.use(function(req,res,next){
-    req.io = io;
-    next();
+app.use(function(req, res, next) {
+        req.io = ioServer;
+        next();
 });
 
-// session secret is controlled by config
-var secret = config.get('global:sessionSecret');
 // compress all responses
 app.use(compression());
-app.use(require("morgan")("combined", { "stream": logger.http.stream }));
-app.use(bodyParser.json({
-  limit: '1mb',
-}));       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     
-  extended: true,
-  limit: '1mb',
-})); // to support URL-encoded bodies
+app.use(morgan('combined', { stream: logger.http.stream }));
+app.use(
+        bodyParser.json({
+                limit: '1mb',
+        })
+); // to support JSON-encoded bodies
+
+app.use(
+        bodyParser.urlencoded({
+                extended: true,
+                limit: '1mb',
+        })
+); // to support URL-encoded bodies
+
 app.use(cookieParser());
 
-var sessSet = {
-    cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 }, // 1 week
-    store: new SQLiteStore,
-    saveUninitialized: true,
-    resave: 'true',
-    secret: secret
-}
+const sessionSettings = {
+        cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 }, // 1 week
+        store: new SQLiteStore(),
+        saveUninitialized: true,
+        resave: 'true',
+        secret: config.get('global:sessionSecret'),
+};
 
-if (process.env.HOSTNAME && process.env.USE_COOKIE_HOST)
-    sessSet.cookie.domain = '.'+process.env.HOSTNAME;
+if (process.env.HOSTNAME && process.env.USE_COOKIE_HOST) sessionSettings.cookie.domain = `.${process.env.HOSTNAME}`;
 
-app.use(session(sessSet));
+app.use(session(sessionSettings));
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash());
-app.use(express.static(path.join(__dirname,'themes',theme, 'public')));
+app.use(express.static(path.join(__dirname, 'themes', config.get('global:theme'), 'public')));
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 app.use(function(req, res, next) {
-  res.locals.version = version;
-  res.locals.loglevel = config.get('global:loglevel') || 'info';
-  next();
+        res.locals.version = version;
+        res.locals.loglevel = config.get('global:loglevel') || 'info';
+        next();
 });
 
 // Import and add routes
@@ -162,117 +161,123 @@ app.use('/post', routes.apiRoute);
 app.use('/api', routes.apiRoute);
 app.use('/auth', routes.authRoute);
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  var title = config.get('global:monitorName') || 'PagerMon';
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  //these 3 have to be here to stop the error handler shitting up the logs with undefined references when it receives a 500 error ... nfi why
-  res.locals.login = req.isAuthenticated();
-  res.locals.gaEnable = config.get('monitoring:gaEnable');
-  res.locals.monitorName = config.get("global:monitorName");
-  res.locals.register = config.get('auth:registration')
+        // set locals, only providing error in development
+        res.locals.message = err.message;
+        res.locals.error = req.app.get('env') === 'development' ? err : {};
+        // these 3 have to be here to stop the error handler shitting up the logs with undefined references when it receives a 500 error ... nfi why
+        res.locals.login = req.isAuthenticated();
+        res.locals.gaEnable = config.get('monitoring:gaEnable');
+        res.locals.monitorName = config.get('global:monitorName');
+        res.locals.register = config.get('auth:registration');
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render(path.join(__dirname,'themes',theme, 'views', 'global', 'error'), { title: title });
+        // render the error page
+        res.status(err.status || 500);
+        res.render(path.join(__dirname, 'themes', config.get('global:theme'), 'views', 'global', 'error'), {
+                title: config.get('global:monitorName'),
+        });
 });
 
 // Add cronjob to automatically refresh aliases
-var dbtype = config.get('database:type')
-if (dbtype == 'mysql') {
-  var aliasRefreshJob = require('cron').CronJob;
-  new aliasRefreshJob('0 5,35 * * * *', function() {
-    var refreshRequired = config.get('database:aliasRefreshRequired')
-    logger.main.debug('CRON: Running Cronjob AliasRefresh')
-    if (refreshRequired == 1) {
-      console.time('updateMap');
-      logger.main.info('CRON: Alias Refresh required, running.')
-      db('messages').update('alias_id', function() {
-        this.select('id')
-            .from('capcodes')
-            .where(db.ref('messages.address'), 'like', db.ref('capcodes.address') )
-            .orderByRaw("REPLACE(address, '_', '%') DESC LIMIT 1")
-      })
-      .then((result) => {
-          console.timeEnd('updateMap');
-          config.set('database:aliasRefreshRequired', 0);
-          config.save();
-          logger.main.info('CRON: Alias Refresh Successful')
-      })
-      .catch((err) => {
-        logger.main.error('CRON: Error refreshing aliases' + err); 
-        console.timeEnd('updateMap'); 
-      })
-    } else {
-      logger.main.debug('CRON: Alias Refresh not Required, Skipping.')
-    }
-  }, null, true);
+const dbtype = config.get('database:type');
+if (dbtype === 'mysql') {
+        /* eslint-disable global-require, no-new */
+        const AliasRefreshJob = require('cron').CronJob;
+        new AliasRefreshJob(
+                '0 5,35 * * * *',
+                function() {
+                        const refreshRequired = config.get('database:aliasRefreshRequired');
+                        logger.main.debug('CRON: Running Cronjob AliasRefresh');
+
+                        if (refreshRequired === 1) {
+                                console.time('updateMap');
+                                logger.main.info('CRON: Alias Refresh required, running.');
+                                db('messages')
+                                        .update('alias_id', function() {
+                                                this.select('id')
+                                                        .select('id')
+                                                        .from('capcodes')
+                                                        .where(
+                                                                db.ref('messages.address'),
+                                                                'like',
+                                                                db.ref('capcodes.address')
+                                                        )
+                                                        .orderByRaw("REPLACE(address, '_', '%') DESC LIMIT 1");
+                                        })
+                                        .then(() => {
+                                                console.timeEnd('updateMap');
+                                                config.set('database:aliasRefreshRequired', 0);
+                                                config.save();
+                                                logger.main.info('CRON: Alias Refresh Successful');
+                                        })
+                                        .catch(err => {
+                                                logger.main.error(`CRON: Error refreshing aliases ${err}`);
+                                                console.timeEnd('updateMap');
+                                        });
+                        } else {
+                                logger.main.debug('CRON: Alias Refresh not Required, Skipping.');
+                        }
+                },
+                null,
+                true
+        );
+        /* eslint-enable global-require, no-new */
 }
 
-//Disable all logging for tests
-if(process.env.NODE_ENV === 'test') { 
-  logger.main.silent = true
-  logger.auth.silent = true
-  logger.db.silent = true
-  logger.http.silent = true
+// Disable all logging for tests
+if (process.env.NODE_ENV === 'test') {
+        logger.main.silent = true;
+        logger.auth.silent = true;
+        logger.db.silent = true;
+        logger.http.silent = true;
 }
 
 module.exports = app;
 
 function normalizePort(val) {
-  var port = parseInt(val, 10);
+        const portToNormalize = parseInt(val, 10);
 
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
+        if (Number.isNaN(portToNormalize))
+                // named pipe
+                return val;
 
-  if (port >= 0) {
-    // port number
-    return port;
-  }
+        if (portToNormalize >= 0)
+                // port number
+                return portToNormalize;
 
-  return false;
+        return false;
 }
 
 function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
+        if (error.syscall !== 'listen') throw error;
 
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+        const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
 
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
+        // handle specific listen errors with friendly messages
+        switch (error.code) {
+                case 'EACCES':
+                        console.error(`${bind} requires elevated privileges`);
+                        process.exit(1);
+                        break;
+                case 'EADDRINUSE':
+                        console.error(`${bind} is already in use`);
+                        process.exit(1);
+                        break;
+                default:
+                        throw error;
+        }
 }
 
 function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-    logger.main.info('Listening on ' + bind);
+        var addr = server.address();
+        var bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
+        logger.main.info(`Listening on ${bind}`);
 }
