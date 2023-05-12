7fa80c515fac3386ba4ec3a8acef97c4a5d23980
alphaO4 <48568909+alphaO4@users.noreply.github.com>
2023-05-12 22:50:35 +0200

    Fix for Telegram Plugin
    This fix allows a person to define multiple telegram ID's in the settings, separated by a ",".
    This is particular useful, in case you don't want to create unnecessary Telegram groups.

--- server/plugins/Telegram.js
+++ server/plugins/Telegram.js
@@ -1,3 +1,4 @@
+                                                                                               
 var telegram = require('telegram-bot-api');
 var util = require('util');
 var logger = require('../log');
@@ -7,32 +8,42 @@ function run(trigger, scope, data, config, callback) {
     if (tConf && tConf.enable) {
         var telekey = config.teleAPIKEY;
         var t = new telegram({
-          token: telekey
+            token: telekey
         });
         if (tConf.chat == 0 || !tConf.chat) {
             logger.main.error('Telegram: ' + data.address + ' No ChatID key set. Please enter ChatID.');
             callback();
         } else {
             //Notification formatted in Markdown for pretty notifications
-            var notificationText = `*${data.agency} - ${data.alias}*\n` + 
-                                    `Message: ${data.message}`;
-            
-            t.sendMessage({
-                chat_id: tConf.chat,
-                text: notificationText,
-                parse_mode: "Markdown"
-            }).then(function(data) {
-                logger.main.debug('Telegram: ' + util.inspect(data, false, null));
-                callback();
-            }).catch(function(err) {
-                logger.main.error('Telegram: ' + err);
-                callback();
-            });
-        }
+            var notificationText = `*${data.agency} - ${data.alias}*\n` +
+                `Message: ${data.message}`;
+            logger.main.info('Sending to the following Chats ' + tConf.chat);
+
+            var chatIds = tConf.chat.split(',');
+            chatIds.forEach(function (chat_id) {
+                sendMessage(chat_id.trim(), notificationText, t);
+		});
+	    callback();
+       }
     } else {
         callback();
     }
 }
+
+function sendMessage(chat_id, notificationText, t) {
+    logger.main.info('current.id: ' + chat_id);
+    t.sendMessage({
+        chat_id: chat_id,
+        text: notificationText
+    }).then(function (data) {
+        logger.main.debug('Telegram: ' + util.inspect(data, false, null));
+    }).catch(function (err) {
+        logger.main.error('Telegram: ' + err);
+    });
+}
+
+
+
 
 module.exports = {
     run: run
