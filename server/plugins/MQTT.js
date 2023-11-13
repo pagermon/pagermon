5a72e7853ddd321641db11df1cd16c31aabc651a
Christopher Haglund <choffa@haglund.uk>
2023-11-13 14:32:24 +0100

    Add some logging to MQTT plugin

--- server/plugins/MQTT.js
+++ server/plugins/MQTT.js
@@ -1,14 +1,24 @@
 const mqtt = require('mqtt');
+const logger = require('../log');
 
 function run(trigger, scope, data, config, callback) {
     const mConf = data.pluginconf.MQTT;
     if (mConf && mConf.enable) {
+        logger.main.debug('MQTT: connecting to MQTT server');
+
         const client = mqtt.connect(config.URL, {
             username: config.username,
             password: config.password,
         });
 
+        client.on('error', error => {
+            logger.main.error(`MQTT: failed to connect to MQTT server: ${error}`);
+            callback();
+        });
+
         client.on('connect', () => {
+            logger.main.debug('MQTT: connected to MQTT server');
+
             const baseTopic = config.baseTopic || 'pagermon';
             const alias = data.alias;
             const aliasId = data.alias_id;
@@ -25,13 +35,15 @@ function run(trigger, scope, data, config, callback) {
                 color: data.color,
             };
 
+            logger.main.debug('MQTT: publishing messages');
+
             // Publish latest message
             client.publish(`${baseTopic}`, JSON.stringify(stateData));
 
             // Publish latest message per alias
             client.publish(`${baseTopic}/${aliasId}`, JSON.stringify(stateData));
 
-            if (config.homeAssistant === 1) {
+            if (config.homeAssistant === true) {
                 // If home assistant integration activated, send discovery message
                 const discoveryTopic = config.discoveryTopic || 'homeassistant';
                 const device = {
@@ -39,6 +51,8 @@ function run(trigger, scope, data, config, callback) {
                     model: 'PagerMon',
                     name: 'PagerMon',
                 };
+
+                logger.main.debug('MQTT: publishing home assistant discovery messages');
 
                 // Configuration for latest message
                 client.publish(
@@ -68,6 +82,8 @@ function run(trigger, scope, data, config, callback) {
                     { retain: true }
                 );
             }
+
+            logger.main.debug('MQTT: messages published');
 
             client.end();
             callback();
