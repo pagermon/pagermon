03fb7f11056c9cfa74309f42e333e750034520da
eopo <26900313+eopo@users.noreply.github.com>
2024-04-15 01:51:47 +0200

    Add more test cases for message search

--- server/knex/seeds/test_data.js
+++ server/knex/seeds/test_data.js
@@ -1,112 +1,129 @@
-
-exports.seed = function(db, Promise) {
-  // Deletes ALL existing entries
-  return db('messages').del() // Deletes ALL existing entries
-    .then(function() { // Inserts seed entries one by one in series
-      return db('messages').insert({
-        address: '1234567',
-        message: 'This is a Test Message to Address 1234567',
-        source: 'Client 1',
-        timestamp: '1529487722'
-      });
-    }).then(function () {
-      return db('messages').insert({
-        address: '1234567',
-        message: 'This is another Test Message to Address 1234567',
-        source: 'Client 2',
-        timestamp: '1529488007'
-      });
-    }).then(function () {
-      return db('messages').insert({
-        address: '1234568',
-        message: 'This is a Test Message to Address 1234568',
-        source: 'Client 1',
-        timestamp: '1529489509'
-      });
-    }).then(function () {
-      return db('messages').insert({
-        address: '1234569',
-        message: 'This is a Test Message to Address 1234569',
-        source: 'Client 3',
-        timestamp: '1529495672'
-      });
-    }).then(function () {
-      return db('messages').insert({
-        address: '1234570',
-        message: 'This is a Test Message to Address 1234570',
-        source: 'Client 4',
-        timestamp: '1529494321'
-      });
-    }).then(function () {
-      return db('capcodes').del()
-    }).then(function () {
-      return db('capcodes').insert({
-        address: '1234567',
-        alias: 'Fire Brigade',
-        agency: 'FIRE',
-        icon: 'fire',
-        color: 'red',
-        ignore: '0',
-      });
-    }).then(function () {
-      return db('capcodes').insert({
-        address: '1234568',
-        alias: 'Ambulance 1',
-        agency: 'AMBULANCE',
-        icon: 'ambulance',
-        color: 'green',
-        ignore: '0',
-      });
-    }).then(function () {
-      return db('capcodes').insert({
-        address: '1234569',
-        alias: 'Police Station',
-        agency: 'POLICE',
-        icon: 'gavel',
-        color: 'blue',
-        ignore: '0',
-      });
-    }).then(function () {
-      return db('capcodes').insert({
-        address: '1234570',
-        alias: 'Ignore Capcode',
-        agency: 'IGNORE',
-        icon: '',
-        color: '',
-        ignore: '1',
-      });
-    }).then(function () {
-      return db('users').del()
-    }).then(function () {
-      return db('users').insert({
-        givenname: 'Active',
-        surname: 'User',
-        username: 'useractive',
-        password: '$2a$08$De/aXnQkZIEbQ9p8J22tHuzLltqIbsAxE2CGgRMPLaaIwwHmVrpsu',
-        email: 'none1@none.com',
-        role: 'user',
-        status: 'active',
-      });
-    }).then(function () {
-      return db('users').insert({
-        givenname: 'Active',
-        surname: 'Admin',
-        username: 'adminactive',
-        password: '$2a$08$De/aXnQkZIEbQ9p8J22tHuzLltqIbsAxE2CGgRMPLaaIwwHmVrpsu',
-        email: 'none2@none.com',
-        role: 'admin',
-        status: 'active',
-      });
-    }).then(function () {
-      return db('users').insert({
-        givenname: 'Disabled',
-        surname: 'Admin',
-        username: 'admindisabled',
-        password: '$2a$08$De/aXnQkZIEbQ9p8J22tHuzLltqIbsAxE2CGgRMPLaaIwwHmVrpsu',
-        email: 'none3@none.com',
-        role: 'admin',
-        status: 'disabled',
-      });
-    })
+exports.seed = function (db, Promise) {
+        // Deletes ALL existing entries
+        return db('messages')
+                .del() // Deletes ALL existing entries
+                .then(() => db('capcodes').del())
+                .then(() =>
+                        db('capcodes').insert({
+                                address: '1234567',
+                                alias: 'Fire Brigade',
+                                agency: 'FIRE',
+                                icon: 'fire',
+                                color: 'red',
+                                ignore: '0',
+                        })
+                )
+                .then(() =>
+                        db('capcodes').insert({
+                                address: '1234568',
+                                alias: 'Ambulance 1',
+                                agency: 'AMBULANCE',
+                                icon: 'ambulance',
+                                color: 'green',
+                                ignore: '0',
+                        })
+                )
+                .then(() =>
+                        db('capcodes').insert({
+                                address: '1234569',
+                                alias: 'Police Station',
+                                agency: 'POLICE',
+                                icon: 'gavel',
+                                color: 'blue',
+                                ignore: '0',
+                        })
+                )
+                .then(() =>
+                        db('capcodes').insert({
+                                address: '1234570',
+                                alias: 'Ignore Capcode',
+                                agency: 'IGNORE',
+                                icon: '',
+                                color: '',
+                                ignore: '1',
+                        })
+                )
+                .then(() =>
+                        // Inserts seed entries one by one in series
+                        db('messages').insert({
+                                address: '1234567',
+                                message: 'This is a Test Message to Address 1234567',
+                                source: 'Client 1',
+                                timestamp: '1529487722',
+                        })
+                )
+                .then(() =>
+                        db('messages').insert({
+                                address: '1234567',
+                                message: 'This is another Test Message to Address 1234567',
+                                source: 'Client 2',
+                                timestamp: '1529488007',
+                        })
+                )
+                .then(() =>
+                        db('messages').insert({
+                                address: '1234568',
+                                message: 'This is a Test Message to Address 1234568',
+                                source: 'Client 1',
+                                timestamp: '1529489509',
+                        })
+                )
+                .then(() =>
+                        db('messages').insert({
+                                address: '1234569',
+                                message: 'This is a Test Message to Address 1234569',
+                                source: 'Client 3',
+                                timestamp: '1529495672',
+                        })
+                )
+                .then(() =>
+                        db('messages').insert({
+                                address: '1234570',
+                                message: 'This is a Test Message to Address 1234570',
+                                source: 'Client 4',
+                                timestamp: '1529494321',
+                        })
+                )
+                .then(() =>
+                        db('messages').insert({
+                                address: '1234572',
+                                message: 'This is a Test Message to non-stored Address 1234572',
+                                source: 'Client 4',
+                                timestamp: '1529494321',
+                        })
+                )
+                .then(() => db('users').del())
+                .then(() =>
+                        db('users').insert({
+                                givenname: 'Active',
+                                surname: 'User',
+                                username: 'useractive',
+                                password: '$2a$08$De/aXnQkZIEbQ9p8J22tHuzLltqIbsAxE2CGgRMPLaaIwwHmVrpsu',
+                                email: 'none1@none.com',
+                                role: 'user',
+                                status: 'active',
+                        })
+                )
+                .then(() =>
+                        db('users').insert({
+                                givenname: 'Active',
+                                surname: 'Admin',
+                                username: 'adminactive',
+                                password: '$2a$08$De/aXnQkZIEbQ9p8J22tHuzLltqIbsAxE2CGgRMPLaaIwwHmVrpsu',
+                                email: 'none2@none.com',
+                                role: 'admin',
+                                status: 'active',
+                        })
+                )
+                .then(() =>
+                        db('users').insert({
+                                givenname: 'Disabled',
+                                surname: 'Admin',
+                                username: 'admindisabled',
+                                password: '$2a$08$De/aXnQkZIEbQ9p8J22tHuzLltqIbsAxE2CGgRMPLaaIwwHmVrpsu',
+                                email: 'none3@none.com',
+                                role: 'admin',
+                                status: 'disabled',
+                        })
+                );
 };
-
--- server/test/routes.api.messagesearch.test.js
+++ server/test/routes.api.messagesearch.test.js
@@ -32,7 +32,7 @@ beforeEach(() => db.migrate.rollback().then(() => db.migrate.latest().then(() =>
 afterEach(() => db.migrate.rollback().then(() => passportStub.logout()));
 
 describe('GET /api/messageSearch', () => {
-        it('should return resut for existing term', done => {
+        it('should return result for existing term', (done) => {
                 chai.request(server)
                         .get('/api/messageSearch?q=test')
                         .end((err, res) => {
@@ -49,9 +49,84 @@ describe('GET /api/messageSearch', () => {
                                 done();
                         });
         });
-        it('should not return resut for non-existing term', done => {
+        it('should not return result for non-existing term', (done) => {
                 chai.request(server)
                         .get('/api/messageSearch?q=thisisnotmessage')
+                        .end((err, res) => {
+                                should.not.exist(err);
+                                res.status.should.eql(200);
+                                res.type.should.eql('application/json');
+                                res.body.should.be.a('object');
+                                should.not.exist(res.body.messages[0]);
+                                done();
+                        });
+        });
+        it('should return result for existing alias', (done) => {
+                chai.request(server)
+                        .get('/api/messageSearch?alias=0')
+                        .end((err, res) => {
+                                should.not.exist(err);
+                                res.status.should.eql(200);
+                                res.type.should.eql('application/json');
+                                res.body.should.be.a('object');
+                                res.body.messages[0].should.have.property('id').eql(0);
+                                res.body.messages[0].should.have.property('address').eql('1234567');
+                                res.body.messages[0].should.have
+                                        .property('message')
+                                        .eql('This is a Test Message to Address 1234567');
+                                res.body.messages[0].should.have.property('source').eql('Client 1');
+                                done();
+                        });
+        });
+        it('should return result for message missing alias', (done) => {
+                chai.request(server)
+                        .get('/api/messageSearch?alias=-1')
+                        .end((err, res) => {
+                                should.not.exist(err);
+                                res.status.should.eql(200);
+                                res.type.should.eql('application/json');
+                                res.body.should.be.a('object');
+                                res.body.messages[0].should.have.property('id').eql(6);
+                                res.body.messages[0].should.have.property('address').eql('1234572');
+                                res.body.messages[0].should.have
+                                        .property('message')
+                                        .eql('This is a Test Message to non-stored Address 1234572');
+                                res.body.messages[0].should.have.property('source').eql('Client 4');
+                                done();
+                        });
+        });
+        it('should not return result for non-existing alias', (done) => {
+                chai.request(server)
+                        .get('/api/messageSearch?alias=18')
+                        .end((err, res) => {
+                                should.not.exist(err);
+                                res.status.should.eql(200);
+                                res.type.should.eql('application/json');
+                                res.body.should.be.a('object');
+                                should.not.exist(res.body.messages[0]);
+                                done();
+                        });
+        });
+        it('should return result for existing address', (done) => {
+                chai.request(server)
+                        .get('/api/messageSearch?address=1234569')
+                        .end((err, res) => {
+                                should.not.exist(err);
+                                res.status.should.eql(200);
+                                res.type.should.eql('application/json');
+                                res.body.should.be.a('object');
+                                res.body.messages[0].should.have.property('id').eql(4);
+                                res.body.messages[0].should.have.property('address').eql('1234569');
+                                res.body.messages[0].should.have
+                                        .property('message')
+                                        .eql('This is a Test Message to Address 1234569');
+                                res.body.messages[0].should.have.property('source').eql('Client 3');
+                                done();
+                        });
+        });
+        it('should not return result for non-existing address', (done) => {
+                chai.request(server)
+                        .get('/api/messageSearch?address=1234585')
                         .end((err, res) => {
                                 should.not.exist(err);
                                 res.status.should.eql(200);
